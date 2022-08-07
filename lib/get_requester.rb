e9093c7da8ace605b71c530367859f570df39528
+ <+>
2022-08-07 11:45:47 -0700

    done

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+class GetRequester
+    attr_accessor :url
+    require 'net/http'
+    require 'open-uri'
+    require 'json'
+
+    def initialize(url)
+        @url = url     
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+    
+
+    def parse_json
+    json = get_response_body
+    JSON(json)
+    end
+
+end
